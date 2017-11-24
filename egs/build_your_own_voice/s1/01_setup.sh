0441a6a58fe654add330c367539e1ea60483ccc9
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-11-24 20:24:05 +0100

    Setup more strict

--- egs/build_your_own_voice/s1/01_setup.sh
+++ egs/build_your_own_voice/s1/01_setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-if test "$#" -ne 1; then
+# Command line part
+if test "$#" -ne 3; then
     echo "################################"
     echo "Usage:"
-    echo "./01_setup.sh <voice_name>"
+    echo "./01_setup.sh <voice_name> <align> <wav_dir>"
     echo ""
     echo "Give a voice name eg., slt_arctic"
+    echo "align can be \"state\" or \"phone\""
+    echo "default path to wav dir(Input): database/wav"
     echo "################################"
     exit 1
 fi
 
+voice_name=$1
+align_type=$2
+wav_dir=$3
+
+# Directory architecture
 current_working_dir=$(pwd)
 merlin_dir=$(dirname $(dirname $(dirname $current_working_dir)))
 experiments_dir=${current_working_dir}/experiments
 data_dir=${current_working_dir}/database
 
-voice_name=$1
 voice_dir=${experiments_dir}/${voice_name}
 
 acoustic_dir=${voice_dir}/acoustic_model
@@ -38,57 +45,81 @@ echo "Hi, this is a demo voice from Merlin." > ${synthesis_dir}/txt/test_002.txt
 echo "Hope you guys enjoy free open-source voices from Merlin." > ${synthesis_dir}/txt/test_003.txt
 printf "test_001\ntest_002\ntest_003" > ${synthesis_dir}/test_id_list.scp
 
+### Settings ###
+global_config_template=conf/global_settings.cfg.template
 global_config_file=conf/global_settings.cfg
 
-### default settings ###
-echo "######################################" > $global_config_file
-echo "############# PATHS ##################" >> $global_config_file
-echo "######################################" >> $global_config_file
-echo "" >> $global_config_file
+cp $global_config_template $global_config_file
 
-echo "MerlinDir=${merlin_dir}" >>  $global_config_file
-echo "WorkDir=${current_working_dir}" >>  $global_config_file
-echo "" >> $global_config_file
+# Paths
+sed -i "s%#MERLIN_DIR#%${merlin_dir}%g" $global_config_file
+sed -i "s%#WORK_DIR#%${current_working_dir}%g" $global_config_file
 
-echo "######################################" >> $global_config_file
-echo "############# PARAMS #################" >> $global_config_file
-echo "######################################" >> $global_config_file
-echo "" >> $global_config_file
+# Parameters
+sed -i "s%#VOICE_NAME#%${voice_name}%g" $global_config_file
+sed -i "s%#ALIGN_TYPE#%${align_type}%g" $global_config_file
 
-echo "Voice=${voice_name}" >> $global_config_file
-echo "Labels=state_align" >> $global_config_file
-echo "QuestionFile=questions-radio_dnn_416.hed" >> $global_config_file
-echo "Vocoder=WORLD" >> $global_config_file
-echo "SamplingFreq=16000" >> $global_config_file
-echo "SilencePhone='sil'" >> $global_config_file
-echo "FileIDList=file_id_list.scp" >> $global_config_file
-echo "" >> $global_config_file
+# Corpus
+nb_total=`ls -1 "$wav_dir"/*.wav | wc -l`
+nb_valid=`echo "$nb_total * 5 / 100" | bc`
+nb_test=$nb_valid
+nb_train=`echo "$nb_total - (2*$nb_valid)"|bc`
+sed -i "s%#NB_TRAIN#%${nb_train}%g" $global_config_file
+sed -i "s%#NB_VALID#%${nb_valid}%g" $global_config_file
+sed -i "s%#NB_TEST#%${nb_test}%g" $global_config_file
 
-echo "######################################" >> $global_config_file
-echo "######### No. of files ###############" >> $global_config_file
-echo "######################################" >> $global_config_file
-echo "" >> $global_config_file
+# Tools
+#  - speech tools
+est_dir=${merlin_dir}/tools/speech_tools
+if [[ ! -d ${est_dir} ]]
+then
+    echo "[needed for phone_align] speech tools is not available (you have to run compile_other_tools.sh in the ${merlin_dir}/tools)"
+    if [[ $align_type == "phone" ]]
+    then
+	exit -1
+    fi
+fi
+sed -i "s%#EST_DIR#%${est_dir}%g" $global_config_file
 
-echo "Train=50" >> $global_config_file 
-echo "Valid=5" >> $global_config_file 
-echo "Test=5" >> $global_config_file 
-echo "" >> $global_config_file
+#  - festival
+fest_dir=${merlin_dir}/tools/festival
+if [[ ! -d ${fest_dir} ]]
+then
+    echo "[needed for phone_align] festival is not available (you have to run compile_other_tools.sh in the ${merlin_dir}/tools)"
+    if [[ $align_type == "phone" ]]
+    then
+	exit -1
+    fi
+fi
+sed -i "s%#FEST_DIR#%${fest_dir}%g" $global_config_file
 
-echo "######################################" >> $global_config_file
-echo "############# TOOLS ##################" >> $global_config_file
-echo "######################################" >> $global_config_file
-echo "" >> $global_config_file
+#  - festvox
+festvox_dir=${merlin_dir}/tools/festvox
+if [[ ! -d ${festvox_dir} ]]
+then
+    echo "[needed for phone_align] festvox is not available (you have to run compile_other_tools.sh in the ${merlin_dir}/tools)"
+    if [[ $align_type == "phone" ]]
+    then
+	exit -1
+    fi
+fi
+sed -i "s%#FESTVOX_DIR#%${festvox_dir}%g" $global_config_file
 
-echo "ESTDIR=${merlin_dir}/tools/speech_tools" >> $global_config_file
-echo "FESTDIR=${merlin_dir}/tools/festival" >> $global_config_file
-echo "FESTVOXDIR=${merlin_dir}/tools/festvox" >> $global_config_file
-echo "" >> $global_config_file
-echo "HTKDIR=${merlin_dir}/tools/bin/htk" >> $global_config_file
-echo "" >> $global_config_file
+#  - HTK
+htk_dir=${merlin_dir}/tools/bin/htk/
+if [[ ! -d ${htk_dir} ]]
+then
+    echo "[needed for state_align] htk is not available (you have to run compile_htk.sh in the ${merlin_dir}/tools)"
+    if [[ $align_type == "state" ]]
+    then
+	exit -1
+    fi
+fi
+sed -i "s%#HTK_DIR#%${htk_dir}%g" $global_config_file
+
 
 echo "Step 1:"
 echo "Merlin default voice settings configured in \"$global_config_file\""
 echo "Modify these params as per your data..."
 echo "eg., sampling frequency, no. of train files etc.,"
 echo "setup done...!"
-
